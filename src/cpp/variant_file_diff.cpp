45ef1fec21e223ae903709bdb3f33929a522df49
Phuwanat Sakornsakolpat <repsa@channing.harvard.edu>
2017-03-26 16:04:45 -0400

    fix double

--- src/cpp/variant_file_diff.cpp
+++ src/cpp/variant_file_diff.cpp
@@ -943,8 +943,9 @@ void variant_file::output_discordance_by_site(const parameters &params, variant_
 		else
 			diffsites << "0";
 
+		double de = N_common_called - N_concord_non_missing_nonRR;
 		double discordance = N_discord / double(N_common_called);
-		double nrd = N_discord  double(N_common_called - N_concord_non_missing_nonRR);
+		double nrd = N_discord / double(de);
 		diffsites << "\t" << N_common_called << "\t" << N_discord << "\t" << discordance << "\t" << N_concord_non_missing_nonRR << "\t" << N_concord_non_missing_nonAA << "\t" << nrd;
 		diffsites << endl;
 	}
